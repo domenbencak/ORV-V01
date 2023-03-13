22f50741be607088a0922dc9a7ecb5097b62c3e5
Domen Bencak <domen.bencak@student.um.si>
2023-03-13 23:10:33 +0100

    implemented selectROI function - after opening the camera it freezes the frame and waits for ROI selection and confirmation using ENTER or SPACE

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ def zmanjsaj_sliko(slika):
     # Vrni zmanjšano sliko
     return cv2.resize(slika, (300, 260))
 
+# Začni zajemanje videa, počakaj 1 sekundo (da senzor dobi dovolj svetlobe) nato zajemi prvi frame
+cap = cv2.VideoCapture(0)
+time.sleep(1)
+_, frame = cap.read()
+frame = cv2.flip(frame, 1)
+frame = zmanjsaj_sliko(frame)
+# Zberi ROI od levega kota zgoraj do desnega kota spodaj
+roi = cv2.selectROI(frame, fromCenter=False)
+cv2.destroyAllWindows()
 cap = cv2.VideoCapture(0)
 
 while True:
