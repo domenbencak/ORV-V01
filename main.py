f3da044282e3969734e1bc5cd3734014c7dd29e8
Domen Bencak <domen.bencak@student.um.si>
2023-04-17 19:49:57 +0200

    Vaja02 - Detektorji robov

--- main.py
+++ main.py
@@ -1,123 +1,314 @@
 import cv2
 import numpy as np
-import time
+import scipy
 
 
-def zmanjsaj_sliko(slika):
-    # Vrni zmanjšano sliko
-    return cv2.resize(slika, (720, 480))
+def my_roberts_manual(slika):
+    # Definicija Robertsovih jeder
+    jedro_x = np.array([[1, 0], [0, -1]])
+    jedro_y = np.array([[0, 1], [-1, 0]])
+
+    # Pridobi velikost slike in jedra
+    visina_slike, sirina_slike = slika.shape
+    visina_jedra, sirina_jedra = jedro_x.shape
+
+    # Inicializacija gradienta x in y na 0
+    gradient_x = np.zeros_like(slika)
+    gradient_y = np.zeros_like(slika)
+
+    # Kovolucija jedra
+    for y in range(visina_slike - visina_jedra + 1):
+        for x in range(sirina_slike - sirina_jedra + 1):
+            podslika = slika[y:y + visina_jedra, x:x + sirina_jedra]
+            gradient_x[y + 1, x + 1] = np.sum(jedro_x * podslika)
+            gradient_y[y + 1, x + 1] = np.sum(jedro_y * podslika)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
 
 
-def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
-    # Določi povprečje in standardno deviacijo pikslov izbrane regije na sliki
-    roi_image = slika[int(levo_zgoraj[1]):int(levo_zgoraj[1] + desno_spodaj[1]),
-                int(levo_zgoraj[0]):int(levo_zgoraj[0] + desno_spodaj[0])]
-    avg_color = np.int_(np.average(roi_image, axis=(0, 1)))
-    std_dev = np.int_(np.std(roi_image))
-    return avg_color - std_dev, avg_color + std_dev
+def my_roberts(slika):
+    # Definicija Robertsovih jeder
+    jedro_x = np.array([[1, 0], [0, -1]])
+    jedro_y = np.array([[0, 1], [-1, 0]])
+
+    # Aplikacija jeder na sliko
+    gradient_x = scipy.ndimage.convolve(slika, jedro_x)
+    gradient_y = scipy.ndimage.convolve(slika, jedro_y)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
 
 
-def prestej_piksle_z_barvo_koze(podslika, barva_koze_spodaj, barva_koze_zgoraj):
-    # Stara rešitev (počasno O(N*N))
-    '''
-    width, height, channel = np.int_(podslika.shape)
-    count = 0
-    for row in range(podslika.shape[0]):
-        for col in range(podslika.shape[1]):
-            pixel = podslika[row, col]
-            if (barva_koze_spodaj <= pixel).all() and (pixel <= barva_koze_zgoraj).all():
-                count += 1
-                # print("count: ", count)
-    return count
-    '''
-    # Ustvari bool masko glede na pogoje, ki padejo v podan rang in nato tiste, ki padejo seštej
-    mask = np.logical_and(barva_koze_spodaj <= podslika, podslika <= barva_koze_zgoraj)
-    count = np.sum(mask)
-    return count
+def my_roberts_absolute(slika):
+    # Definicija Robertsovih jeder
+    jedro_x = np.array([[1, 0], [0, -1]])
+    jedro_y = np.array([[0, 1], [-1, 0]])
+
+    # Aplikacija jeder na sliko
+    gradient_x = scipy.ndimage.convolve(slika, jedro_x)
+    gradient_y = scipy.ndimage.convolve(slika, jedro_y)
+
+    # Kalkulacija približne magnitude
+    gradient_mag = np.absolute(gradient_x) + np.absolute(gradient_y)
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= np.uint8(255.0 / gradient_mag.max())
+    gradient_mag = np.uint8(gradient_mag)
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
 
 
-def obdelaj_sliko(slika, okno_sirina, okno_visina, barva_koze_spodaj, barva_koze_zgoraj):
-    najboljse_ujemanje = -1
-    # Nastavi (korak) na 20% širine in višine
-    visina = int(okno_visina * 0.25)
-    sirina = int(okno_sirina * 0.1)
-    # Za okvirčke od začetka do konca, po koraku 10% širine in 25% višine
-    for y in range(0, int(okno_visina), visina):
-        for x in range(0, int(okno_sirina), sirina):
-            # Izreži trenutno okno
-            okno = slika[y:y + visina, x:x + sirina]
-            top_left = (x, y)
-            bottom_right = (x + sirina, y + visina)
-            # Dobi trenutno število pikslov, ki se ujemajo
-            trenutno_ujemanje = prestej_piksle_z_barvo_koze(okno, barva_koze_spodaj, barva_koze_zgoraj)
-            # Preveri ali si prvič v zanki in nato shrani v najboljše ujemanje prvi okvirček
-            if najboljse_ujemanje == -1:
-                najboljse_ujemanje = trenutno_ujemanje
-                najboljse_tl = top_left
-                najboljse_br = bottom_right
-            # Če je trenutno ujemanje boljše od najboljšega do zdaj
-            elif trenutno_ujemanje > najboljse_ujemanje:
-                najboljse_ujemanje = trenutno_ujemanje
-                najboljse_tl = top_left
-                najboljse_br = bottom_right
-    # Za okvirčke od 5% do 95% širine in 12.5% do 87.5% višine, po koraku 10% širine in 25% višine
-    for y in range(int(visina / 2), int(okno_visina - visina / 2), visina):
-        for x in range(int(sirina / 2), int(okno_sirina - visina / 2), sirina):
-            # Izreži trenutno okno
-            okno = slika[y:y + visina, x:x + sirina]
-            top_left = (x, y)
-            bottom_right = (x + sirina, y + visina)
-            # Dobi trenutno število pikslov, ki se ujemajo
-            trenutno_ujemanje = prestej_piksle_z_barvo_koze(okno, barva_koze_spodaj, barva_koze_zgoraj)
-            # Preveri ali si prvič v zanki in nato shrani v najboljše ujemanje prvi okvirček
-            if najboljse_ujemanje == -1:
-                najboljse_ujemanje = trenutno_ujemanje
-                najboljse_tl = top_left
-                najboljse_br = bottom_right
-            # Če je trenutno ujemanje boljše od najboljšega do zdaj
-            elif trenutno_ujemanje > najboljse_ujemanje:
-                najboljse_ujemanje = trenutno_ujemanje
-                najboljse_tl = top_left
-                najboljse_br = bottom_right
+def my_prewitt(slika):
+    # Definicija Prewitt jeder
+    jedro_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    jedro_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 
-    return (najboljse_tl, najboljse_br)
+    # Aplikacija jeder na sliko
+    gradient_x = scipy.ndimage.convolve(slika, jedro_x)
+    gradient_y = scipy.ndimage.convolve(slika, jedro_y)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
 
 
-# Začni zajemanje videa, počakaj 1 sekundo (da senzor dobi dovolj svetlobe) nato zajemi prvi frame
-cap = cv2.VideoCapture(0)
-time.sleep(1)
-_, frame = cap.read()
-frame = cv2.flip(frame, 1)
-frame = zmanjsaj_sliko(frame)
-# Zberi ROI od levega kota zgoraj do desnega kota spodaj in določi levo zgoraj in desno spodaj koordinate
-roi = cv2.selectROI("Izberi predmet za sledenje", frame, fromCenter=False, showCrosshair=False)
-levo_zgoraj = (roi[0], roi[1])
-desno_spodaj = (roi[2], roi[3])
-# Določi barvo kože, ki predstavlja obraz, ki ga iščemo
-bk_spodaj, bk_zgoraj = doloci_barvo_koze(frame, levo_zgoraj, desno_spodaj)
-# Zapri vsa okna in začni nov capture
-cv2.destroyAllWindows()
-cap = cv2.VideoCapture(0)
+def my_prewitt_manual(slika):
+    # Definicija Prewitt jeder
+    jedro_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    jedro_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 
-while True:
-    # Preberi frame iz kamere
-    ret, frame = cap.read()
-    # Obrni frame
-    frame = cv2.flip(frame, 1)
-    # Zmanjšaj sliko
-    frame = zmanjsaj_sliko(frame)
-    # Pridobi višino in širino okna (slike)
-    visina, sirina, channel = np.int_(frame.shape)
-    # Obdelaj sliko in shrani levi zgornji kot in desni spodnji kot regije, ki se najboljše ujema z izbranim
-    topleft, bottomright = obdelaj_sliko(frame, sirina, visina, bk_spodaj, bk_zgoraj)
-    # Nariši okvir na to regijo in prikaži frame + okvir
-    cv2.rectangle(frame, topleft, bottomright, (255, 255, 62), 2)
-    cv2.imshow('Kamera', frame)
+    # Pridobi velikost slike in jedra
+    visina_slike, sirina_slike = slika.shape
+    visina_jedra, sirina_jedra = jedro_x.shape
 
-    # Pritisni 'q' za izhod
-    if cv2.waitKey(1) == ord('q'):
-        break
+    # Inicializacija gradienta x in y na 0
+    gradient_x = np.zeros_like(slika)
+    gradient_y = np.zeros_like(slika)
 
-# Ugasni kamero in uniči vsa okna
-cap.release()
+    # Konvolucija jedra
+    for y in range(visina_slike - visina_jedra + 1):
+        for x in range(sirina_slike - sirina_jedra + 1):
+            podslika = slika[y:y + visina_jedra, x:x + sirina_jedra]
+            gradient_x[y + 1, x + 1] = np.sum(jedro_x * podslika)
+            gradient_y[y + 1, x + 1] = np.sum(jedro_y * podslika)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
+
+
+def my_gaussian(slika, velikost_jedra=3, sigma=1.0):
+    # Ustvari jedro podane velikost in z dano sigmo
+    jedro = np.zeros((velikost_jedra, velikost_jedra))
+    # Pridobi x in y koordinato sredine jedra (deljenje in zaokrožanje navzdol)
+    center = velikost_jedra // 2
+    print(f'center: {center}')
+    for i in range(velikost_jedra):
+        for j in range(velikost_jedra):
+            x, y = i - center, j - center
+            jedro[i, j] = np.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+            print(f'jedro{i},{j}]: {jedro[i, j]}')
+    jedro /= np.sum(jedro)
+
+    # Obloži sliko z ničlami, da na robu ne pride do pokvaritve
+    slika_padded = np.pad(slika, ((center, center), (center, center)), mode='constant')
+
+    # Konvolucija jedra
+    slika_gauss = np.zeros_like(slika)
+    for y in range(slika.shape[0]):
+        for x in range(slika.shape[1]):
+            podslika = slika_padded[y:y + velikost_jedra, x:x + velikost_jedra]
+            slika_gauss[y, x] = np.sum(podslika * jedro)
+
+    return slika_gauss
+
+
+def my_sobel(slika):
+    # V bistvu kombinacija Prewitt in Gaussovega filtra za glajenje
+    slika = my_gaussian(slika)
+    # Definicija Sobel jeder
+    jedro_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    jedro_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+    # Aplikacija jeder na sliko
+    gradient_x = scipy.ndimage.convolve(slika, jedro_x)
+    gradient_y = scipy.ndimage.convolve(slika, jedro_y)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
+
+
+def my_sobel_manual(slika):
+    # V bistvu kombinacija Prewitt in Gaussovega filtra za glajenje
+    slika = my_gaussian(slika)
+    # Definicija Sobel jeder
+    jedro_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    jedro_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
+
+    # Pridobi velikost slike in jedra
+    visina_slike, sirina_slike = slika.shape
+    visina_jedra, sirina_jedra = jedro_x.shape
+
+    # Inicializacija gradienta x in y na 0
+    gradient_x = np.zeros_like(slika)
+    gradient_y = np.zeros_like(slika)
+
+    # Kovolucija jedra
+    for y in range(visina_slike - visina_jedra + 1):
+        for x in range(sirina_slike - sirina_jedra + 1):
+            podslika = slika[y:y + visina_jedra, x:x + sirina_jedra]
+            gradient_x[y + 1, x + 1] = np.sum(jedro_x * podslika)
+            gradient_y[y + 1, x + 1] = np.sum(jedro_y * podslika)
+
+    # Kalkulacija magnitude gradienta
+    gradient_mag = np.sqrt(np.square(gradient_x) + np.square(gradient_y))
+
+    # Normalizacija gradienta magnitude
+    gradient_mag *= 255.0 / gradient_mag.max()
+
+    # Pretvorba gradient magnitude v uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
+
+
+def my_canny(slika, sp_prag, zg_prag):
+    spremenjena_slika = cv2.Canny(slika, threshold1=sp_prag, threshold2=zg_prag)
+    return spremenjena_slika
+
+
+def spremeni_kontrast_overflow(slika, alfa, beta):
+    spremenjena_slika = alfa * slika + beta
+    return spremenjena_slika
+
+
+def spremeni_kontrast_manual(slika, alfa, beta):
+    spremenjena_slika = slika * alfa
+    for row in range(spremenjena_slika.shape[0]):
+        for col in range(spremenjena_slika.shape[1]):
+            pixel = spremenjena_slika[row, col]
+            if pixel + beta > 255:
+                spremenjena_slika[row, col] = 255
+            elif pixel + beta < 0:
+                spremenjena_slika[row, col] = 0
+            else:
+                spremenjena_slika[row, col] += beta
+
+    return spremenjena_slika
+
+
+def spremeni_kontrast_cv2(slika, alfa, beta):
+    output = cv2.convertScaleAbs(slika, alpha=alfa, beta=beta)
+    return output
+
+
+def spremeni_kontrast(slika, alfa, beta):
+    slika = slika.astype('float32')
+    slika = alfa * slika + beta
+    slika = np.clip(slika, 0, 255).astype('uint8')
+    return slika
+
+
+slika = cv2.imread('lenna.png', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('Slika', slika)
+# slika2 = cv2.imread('lenna.png')
+# cv2.imshow('Slika2', slika2)
+
+
+adjusted_img = spremeni_kontrast(slika, alfa=1.5, beta=0)
+cv2.imshow('Spremeni_kontrast 1.5, 0', adjusted_img)
+
+# adjusted_img2 = my_gaussian(adjusted_img)
+# cv2.imshow('Spremeni_kontrast 0.2, 0 + Gaussian blur[3]', adjusted_img2)
+
+
+'''
+adjusted_img2 = spremeni_kontrast_cv2(slika, alfa=2, beta=50)
+cv2.imshow('Adjusted2', adjusted_img2)
+
+adjusted_img3 = spremeni_kontrast(slika, alfa=2, beta=50)
+cv2.imshow('Adjusted3', adjusted_img3)
+
+
+kontrast_slika1 = spremeni_kontrast(slika, alfa=1.5, beta=0)
+# cv2.imshow('Konstrast slika', kontrast_slika)
+
+kontrast_slika9 = spremeni_kontrast(slika, alfa=3, beta=0)
+# cv2.imshow('Konstrast slika2', kontrast_slika2)
+
+
+
+prewitt = my_prewitt_manual(slika)
+cv2.imshow('Prewitt Original', prewitt)
+prewitt2 = my_prewitt_manual(adjusted_img)
+cv2.imshow('Prewitt Manual', prewitt2)
+'''
+'''
+sobel = my_sobel(slika)
+cv2.imshow('Sobel Original', sobel)
+sobel2 = my_sobel_manual(adjusted_img)
+cv2.imshow('Sobel Manual', sobel2)
+'''
+'''
+roberts = my_roberts_manual(slika)
+cv2.imshow('Roberts Original', roberts)
+roberts2 = my_roberts_manual(adjusted_img)
+cv2.imshow('Roberts 1, 120', roberts2)
+'''
+'''
+lowerb1 = 10
+higherb1 = 30
+
+lowerb2 = 10
+higherb2 = 30
+
+canny = my_canny(slika, lowerb1, higherb1)
+cv2.imshow(f'Canny {lowerb1} {higherb1}', canny)
+
+canny2 = my_canny(adjusted_img, lowerb2, higherb2)
+cv2.imshow(f'Canny1 {lowerb2} {higherb2}', canny2)
+'''
+cv2.waitKey(0)
 cv2.destroyAllWindows()
